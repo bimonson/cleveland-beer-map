b06a9b73cb55bcccf1771b2e3f2c7bc479756687
Benjamin Simonson <bfsimon@gmail.com>
2018-12-16 17:01:23 -0500

    creates MapDiv component

---
+++ src/components/MapDiv.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MapDiv extends Component {
+  render() {
+    return (
+      <div
+        id="map"
+        role="application"
+        aria-label="map">
+      </div>
+    );
+  }
+}
+
+export default MapDiv;
